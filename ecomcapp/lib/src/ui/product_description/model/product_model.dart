6726141b753cc599da4043c5af8a85b1b3ef2dd9
Shubham <shubhamtrip16@gmail.com>
2022-12-04 15:58:05 +0530

    model created

---
+++ ecomcapp/lib/src/ui/product_description/model/product_model.dart
@@ -0,0 +1,12 @@
+class ProductModel {
+  final String title;
+  final String description;
+  final double price;
+  final String imgSource;
+
+  ProductModel(
+      {required this.description,
+      required this.imgSource,
+      required this.price,
+      required this.title});
+}
---
+++ ecomcapp/lib/src/ui/route_gengerator.dart
@@ -0,0 +1,22 @@
+import 'package:ecomcapp/src/ui/product_description/product_description.dart';
+import 'package:ecomcapp/src/ui/productsscreen/productsscreen.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.productdes:
+        return MaterialPageRoute(builder: (_) => ProductDescription());
+      case Routes.productsscreen:
+        return MaterialPageRoute(builder: (_) => ProductsScreen());
+
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(child: Text('Something went wrong')),
+                ));
+    }
+  }
+}
