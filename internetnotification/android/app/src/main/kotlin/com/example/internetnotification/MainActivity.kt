a3c951a27d828adbc21428ec4af8042ad1e4525c
Shubham <shubhamtrip16@gmail.com>
2022-12-16 17:16:46 +0530

    deleted un

--- internetnotification/android/app/src/main/kotlin/com/example/internetnotification/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.internetnotification
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- internetnotification/lib/Cubit/internet_tale.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-enum InternetStates { InternetLoading, InternetConnected, InternetDisconnected }
